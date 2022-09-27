aa8ca20722623b4de77d96c73e7f9eb88aa886d2
Andrey Goryachev <raikslerx@gmail.com>
2022-09-27 19:17:05 +0300

    Добавлена переменная окружения. Возможность задать токен по умолчанию или вводить новый при каждом запуске

--- lora_the_dog/main.py
+++ lora_the_dog/main.py
@@ -2,6 +2,7 @@ import random
 import vk_api
 import psycopg2
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import env
 from weather import Weather
 import dialog_const
 
@@ -29,9 +30,17 @@ class Db_manager:
 def write_msg(chat_id, message, reply_to_id=None):
     vk.method('messages.send', {'chat_id' : chat_id, 'message' : message, 'random_id' : 0})
 
-token_vk = input('Paste your VK token here: ')
+if env.vk_token != None:
+    token_vk = env.vk_token
+    print('В переменных окружения обнаружен заданный токен. Он будет использован для текущего запуска бота. Задать другой токен, можно в файле env.py.')
+else:
+    print('В файле окружения не задан токен VK. Будет произведен запрос токена через консоль. Задать токен по умолчанию можно в соответствующей переменной файла env.py. Подробности о получении токена в README.')
+    token_vk = input('Paste your VK token here: ')
+
 vk = vk_api.VkApi(token=token_vk)                                                  # Авторизация вк
 longpoll = VkBotLongPoll(vk, 215943276, wait=25)                                   # Класс для работы с лонгполл сервером вк
+
+print('Бот активен!')
 
 for event in longpoll.listen():                                                    # Основной цикл, прослушивание long poll
     if event.type == VkBotEventType.MESSAGE_NEW and event.object.message['text'][1:14] == 'club215943276':
