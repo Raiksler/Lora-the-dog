2a910c44a79a952ab16e32a290dc1e6fbe587d5e
Andrey Goryachev <raikslerx@gmail.com>
2023-02-10 20:48:04 +0300

    Добавлены утренние прогнозы от Лоры. Обновление команд. Обновление README

--- lora_the_dog/main.py
+++ lora_the_dog/main.py
@@ -2,9 +2,12 @@ import random
 import vk_api
 import psycopg2
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import threading
+import schedule
 import env
 from weather import Weather
 import dialog_const
+
 
 class Db_manager:
     def __init__(self):
@@ -14,6 +17,11 @@ class Db_manager:
     def is_chat_in_db(self, id):
         self.cursor.execute('SELECT id FROM chats;')
         return id in [x[0] for x in self.cursor.fetchall()]
+
+    def get_chats_id(self):
+        self.cursor.execute('SELECT id FROM chats;')
+        ids = self.cursor.fetchall()
+        return ids
     
     def add_chat(self, id):
         self.cursor.execute('INSERT INTO chats (id, mood) VALUES ({chat_id}, 30);'.format(chat_id = id))
@@ -27,8 +35,35 @@ class Db_manager:
         self.cursor.execute("UPDATE chats SET mood={} WHERE id={};".format(mood, id))
         self.connection.commit()
 
+
 def write_msg(chat_id, message, reply_to_id=None):
     vk.method('messages.send', {'chat_id' : chat_id, 'message' : message, 'random_id' : 0})
+
+
+def morning_weather():
+    def weather_forecast():
+        chat_ids = list()
+        for item in Db_manager().get_chats_id():
+            chat_id = item[0]
+            current_mood = Db_manager().get_mood(chat_id)
+            if current_mood > 100:
+                Db_manager().change_mood(chat_id, 100)
+            if current_mood < -50:
+                Db_manager().change_mood(chat_id, -50)
+            try:
+                if current_mood > 0:
+                    write_msg(message = "@all *Лора сладко зевает, пробуждаясь от глубокого сна*\n\nПогодная сводка от Лоры:\n\n" + Weather.get_message('today'), chat_id=chat_id)
+                    Db_manager().change_mood(chat_id, current_mood - 5)
+                else:
+                    write_msg(message = '*Лора грустно зевает, пробуждаясь от беспокойного сна, ее настроение упало слишком низко, чтобы давать прогнозы.*', chat_id=chat_id)
+                    write_msg(message = 'Ты можешь "поиграть с Лорой" или "угостить Лору морковкой", чтобы поднять ее настроение!', chat_id=chat_id)
+            except vk_api.exceptions.ApiError:          #Попытка отправить сообщение в чат, где нет бота.
+                pass
+
+    schedule.every().day.at("08:00").do(weather_forecast)        #Назначаем событие и интервал повторения
+    while True:
+        schedule.run_pending()                                #Запускаем выполнение графика
+
 
 if env.vk_token != None:
     token_vk = env.vk_token
@@ -37,10 +72,14 @@ else:
     print('В файле окружения не задан токен VK. Будет произведен запрос токена через консоль. Задать токен по умолчанию можно в соответствующей переменной файла env.py. Подробности о получении токена в README.')
     token_vk = input('Paste your VK token here: ')
 
+
 vk = vk_api.VkApi(token=token_vk)                                                  # Авторизация вк
 longpoll = VkBotLongPoll(vk, 215943276, wait=25)                                   # Класс для работы с лонгполл сервером вк
 
 print('Бот активен!')
+
+thread = threading.Thread(target=morning_weather)
+thread.start()
 
 for event in longpoll.listen():                                                    # Основной цикл, прослушивание long poll
     if event.type == VkBotEventType.MESSAGE_NEW and event.object.message['text'][1:14] == 'club215943276':
